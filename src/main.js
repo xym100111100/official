08dc498513b68400cad09e79fb580d981fdbf86f
xym100111100 <253274870@qq.com>
2021-04-27 00:25:00 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,75 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+/* jquery */
+
+
+import 'jquery/dist/jquery.min'
+ 
+import 'bootstrap/dist/css/bootstrap.css'
+/* 路由 */
 import router from './router'
-import store from './store'
+
+/* axios */
+import axios from './api'
+import api from './api/api'
+Vue.prototype.http = axios;
+Vue.prototype.api = api;
+
+import 'swiper/dist/css/swiper.min.css';
+
+import 'bootstrap'
+
+
+/* swiper */
+// import 'swiper/dist/css/swiper.min.css';
+
+/* 重置样式 */
+import './assets/css/reset.min.css'
+
+/* bootstarp */
+ import './assets/css/bootstrap.min.css'
+
+
+/* animate.css */
+import animated from 'animate.css'
+
+ 
+/* 头部组件 */
+import Header from './components/Header'
+Vue.component(Header.name,Header)
+import BaiduMap from 'vue-baidu-map'
+
+Vue.use(animated)
+
+/* 尾部组件 */
+import Footer from './components/Footer'
+Vue.component(Footer.name,Footer)
+
+/* 回到顶部 */
+import GoTop from './components/GoTop'
+Vue.component(GoTop.name,GoTop)
 
 Vue.config.productionTip = false
 
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.title){
+      document.title = to.meta.title
+    }
+    next();
+});
+
+
+
+Vue.use(BaiduMap, {
+  // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+  ak: 'fZ8WORpQRgYHyS0VQuTsPhQB10kjc0df'
+})
+
+
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,11 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const state = {
+
+}
+
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state
 })
